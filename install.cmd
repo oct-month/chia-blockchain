8cc7b4acbb86184571b697fc5559fbe2c91a5e95
ablocker <oct-month@qq.com>
2021-05-18 18:57:43 +0800

    '修改脚本'

--- install.cmd
+++ install.cmd
@@ -1,6 +1,8 @@
 conda create python=3.8 --prefix=.\.venv
 conda activate .\.venv
 
+pip install pywin32
+
 python setup.py build
 python setup.py install
-pip install --upgrade pywin32 -I
+@REM pip install --upgrade pywin32 -I
--- plot.cmd
+++ plot.cmd
@@ -1,1 +1,10 @@
-chia plots create -k 32 -n 1 -r 2 -t F:\cache -d E:\plots
+@REM -k 农田大小
+@REM -n 农田个数
+@REM -r 线程数
+@REM -f 农民公钥
+@REM -p 矿池公钥
+@REM -t 缓存位置
+@REM -d 目标位置
+chia plots create -k 32 -n 1 -r 2 -t H:\cache -d G:\farmland ^
+    -f b436e58c14c8685f0ee5f3f90e2ba4c0c8957620179f92941f2b30582ab92fa1101fec5b40133489aeb8d439a73c7ab7 ^
+    -p a5de12358d9465e2189e62fa12e5b75af7e1a252a9abda221550247280ac115ec0586bda8ac38269ba9e6410031ce281
